9e03b4aa695065007a762840836854e60c0c1a30
MarJuice <mariushanss1@gmail.com>
2025-02-03 23:46:09 +0100

    Create multiply function and tests

--- a.mjs
+++ a.mjs
@@ -20,17 +20,53 @@ import test from "./test.mjs";
 //#region function -----------------------------------------------------------------
 // Write your function her.
 
+function multiply(...factors) {
+    // Check if there are no parameters given
+    if (factors.length == 0) {
+        return NaN;
+    } 
+    
+    // Store product of factors in a variable
+    let product = 1;
+    
+    // Loop over each factor
+    for (let num of factors) {
+        if (isNaN(num) || num === null) { // Check if factor is a number
+            return NaN;
+        } else if (typeof num === "string") { // Check if factor is a string -> Number() converts to number, returns NaN if string isn't number.
+            num = Number(num);
+        }
+        product *= num; // Get product of factors
+    }
 
+    return product;
+}
 
 //#endregion
-
-
 
 
 
 //#region Tests --------------------------------------------------------------------
 // Write your tests her.
 
+const tests = test("Multiply function");
 
+// Valid inputs
+tests.isEqual(multiply(1, 2), 2, "1 multiplied by 2 should be 2");
+tests.isEqual(multiply(-5, 5), -25, "-5 multiplied by 5 should be -25");
+tests.isEqual(multiply(2.5, 3.5), 8.75, "2.5 multiplied by 3.5 should be 8.75");
+tests.isEqual(multiply(1, "2"), 2, '1 multiplied by "2" should be 2');
+tests.isEqual(multiply(2, 2, 3), 12, "2 multiplied by 2 multiplied by 3 should be 12");
+
+// Invalid inputs
+tests.isNotANumber(multiply(), "No input should return NaN"); 
+tests.isNotANumber(multiply(1, null), "1 multiplied by null should return NaN"); 
+tests.isNotANumber(multiply(undefined, 3), "undefined multiplied by 3 should return NaN");
+tests.isNotANumber(multiply(NaN, 3), "NaN multiplied by 3 should return NaN");
+
+// Edge cases
+tests.isEqual(multiply(0, 0), 0, "0 multiplied by 0 should be 0");
+tests.isEqual(multiply(Infinity, 1), Infinity, "Infinity multiplied by 1 should be Infinity");
+tests.isEqual(multiply(-Infinity, 1), -Infinity, "-Infinity multiplied by 1 should be -Infinity");
 
 //#endregion
