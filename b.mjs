ffabc169e18f20c5ea804a8e759a9026846a7844
MarJuice <mariushanss1@gmail.com>
2025-02-05 22:59:55 +0100

    Create formatName function and tests

--- b.mjs
+++ b.mjs
@@ -20,7 +20,35 @@ import test from "./test.mjs";
 //#region function -----------------------------------------------------------------
 // Write your function her.
 
+function formatName(name) {
+    // Check if input is string
+    if (typeof name !== "string") {
+        return null;
+    }
 
+    // Remove whitespaces from both ends
+    name = name.trim();
+
+    // Check if string contains letters
+    if (name.length == 0) {
+        return " ";
+    }
+
+    const specialCharacters = "!@#$%^&*()_+-=[]{}|;:',.<>/?`~0123456789"; // String of characters to compare name with
+    const formattedName = name.toLowerCase().split(" "); // Turn entire string to lower case and separate names by space
+
+    for (let i = 0; i < formattedName.length; i++) {
+        for (let character of formattedName[i]) { // Check if name contains special character
+            if (specialCharacters.includes(character)) {
+                return null;
+            }
+        }        
+        // Replace initial letter with upper case letter and re-add the rest of the word
+        formattedName[i] = formattedName[i].charAt(0).toUpperCase() + formattedName[i].slice(1);
+    }
+
+    return formattedName.join(" "); // Return the formatted string
+}
 
 //#endregion
 
@@ -31,6 +59,20 @@ import test from "./test.mjs";
 //#region Tests --------------------------------------------------------------------
 // Write your tests her.
 
+const tests = test("Format name function");
 
+// Valid inputs
+tests.isEqual(formatName("marius hanssen"), "Marius Hanssen", "Lower case initial letters should be capitalized");
+tests.isEqual(formatName(""), " ", "Empty input should return an empty string");
+
+// Invalid inputs
+tests.isEqual(formatName(123), null, "Formatting numbers should return null");
+tests.isEqual(formatName(null), null, "Formatting null should return null");
+tests.isEqual(formatName(undefined), null, "Formatting undefined should return null");
+tests.isEqual(formatName("Marius 123"), null, "Strings with special characters should return null");
+
+// Edge cases
+tests.isEqual(formatName("MARIUS HANSSEN"), "Marius Hanssen", "Only initial letters should be capitalized");
+tests.isEqual(formatName("   Marius Hanssen   "), "Marius Hanssen", "Trailing whitespaces should be removed");
 
 //#endregion
